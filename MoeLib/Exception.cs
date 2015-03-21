470119053114b1ad18bd8ff8f0cca32d78769a5f
Siqi Lu <lu.siqi@outlook.com>
2015-03-21 17:24:17 +0800

    add exception extensions

---
+++ MoeLib/Exception.cs
@@ -0,0 +1,76 @@
+﻿// ***********************************************************************
+// Assembly         : MoeLib
+// Author           : Siqi Lu
+// Created          : 2015-03-21  5:13 PM
+//
+// Last Modified By : Siqi Lu
+// Last Modified On : 2015-03-21  5:23 PM
+// ***********************************************************************
+// <copyright file="Exception.cs" company="Shanghai Yuyi">
+//     Copyright ©  2012-2015 Shanghai Yuyi. All rights reserved.
+// </copyright>
+// ***********************************************************************
+
+using System;
+using System.Text;
+
+namespace Moe.Lib
+{
+    /// <summary>
+    ///     Extensions of <see cref="System.Exception" /> type.
+    /// </summary>
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        ///     Gets the exception string.
+        /// </summary>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        public static string GetExceptionString(this Exception e)
+        {
+            return CreateExceptionString(e);
+        }
+
+        /// <summary>
+        ///     Creates the exception string.
+        /// </summary>
+        /// <param name="e">The exception.</param>
+        /// <returns>System.String.</returns>
+        private static string CreateExceptionString(Exception e)
+        {
+            StringBuilder sb = new StringBuilder();
+            CreateExceptionString(sb, e, string.Empty);
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        ///     Creates the exception string.
+        /// </summary>
+        /// <param name="sb">The string builder.</param>
+        /// <param name="e">The exception.</param>
+        /// <param name="indent">The indent string.</param>
+        private static void CreateExceptionString(StringBuilder sb, Exception e, string indent)
+        {
+            if (indent == null)
+            {
+                indent = string.Empty;
+            }
+            else if (indent.Length > 0)
+            {
+                sb.AppendFormat("{0}Inner ", indent);
+            }
+
+            sb.AppendFormat("Exception Found:\n{0}Type: {1}", indent, e.GetType().FullName);
+            sb.AppendFormat("\n{0}Message: {1}", indent, e.Message);
+            sb.AppendFormat("\n{0}Source: {1}", indent, e.Source);
+            sb.AppendFormat("\n{0}Stacktrace: {1}", indent, e.StackTrace);
+
+            if (e.InnerException != null)
+            {
+                sb.Append("\n");
+                CreateExceptionString(sb, e.InnerException, indent + "  ");
+            }
+        }
+    }
+}
